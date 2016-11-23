bd0a39d06342a6b82894ad8ba84d1f5e710f8bc4
Kalen Peterson <kpeterson@forsythe.com>
2016-11-23 13:31:30 -0600

    Completed update of pe_master_backup.sh to make it more robust and keep backup in a single file

--- pe_master_backup.sh
+++ pe_master_backup.sh
@@ -12,27 +12,58 @@
 #  Written by: Kalen Peterson <kpeterson@forsythe.com> 
 #  Created on: 11/23/2016
 
-BACKUP_DIR=/var/puppet/backups
+# Set Initial Variables
+BACKUP_DIR="$1"
 TIMESTAMP=`date +"%m%d%Y-%H%M%S"`
-FILE_BACKUP="$BACKUP_DIR/pe_backup.$TIMESTAMP.tar.gz"
-SQL_BACKUP="$BACKUP_DIR/pe_sql_backup.sql"
+ARCHIVE_FILE="pe_backup.$TIMESTAMP.tar.gz"
+SQL_FILE="pe_sql_backup.sql"
 
+
+#############
+## Functions
+#############
+# Print Script Usage
+F_Usage () {
+  echo
+  echo "Usage: pe_master_backup.sh /path/to/backup/dir"
+  echo
+  echo "This script will backup a PE Master to an archive"
+  echo "from which it can be restored." 
+  exit 2
+}
+
+
+##############
+## Validation
+##############
+# Validate that we are root
 if [[ `id -u` -ne 0 ]]; then
-  echo "You must be root!"
-  exit 1
+  echo
+  echo "ERROR: You must be root!"
+  F_Usage
 fi
 
+# Ensure we passed a backup directory
+if [[ -z "$BACKUP_DIR" ]]; then
+  echo
+  echo "ERROR: No Backup Directory specified"
+  F_Usage
+fi
+
+# Ensure the backup directory exists
 if [[ ! -d "$BACKUP_DIR" ]]; then
   mkdir -p "$BACKUP_DIR"
 fi
 
 # Cleanup temp files
-rm -f "$SQL_BACKUP"
-function F_Exit {
-  rm -f "$SQL_BACKUP"
+rm -f "$BACKUP_DIR/$SQL_FILE"
+F_Exit () {
+  rm -f "$BACKUP_DIR/$SQL_FILE"
 }
-trap F_Exit EXIT
 
+##################
+## Perform Backup
+##################
 # Keep a copy of /etc/puppetlabs in $BACKUP_DIR
 echo
 echo "Start raw copy of /etc/puppetlabs"
@@ -41,18 +72,27 @@ cp -rp /etc/puppetlabs "$BACKUP_DIR"
 # Backup the PuppetDB
 echo
 echo "Starting DB Dump"
-touch "$SQL_BACKUP"; chown pe-postgres: "$SQL_BACKUP"; chmod 640 "$SQL_BACKUP"
-sudo -u pe-postgres /opt/puppetlabs/server/apps/postgresql/bin/pg_dumpall -c -f "$SQL_BACKUP"
+touch "$BACKUP_DIR/$SQL_FILE"
+chown pe-postgres: "$BACKUP_DIR/$SQL_FILE"
+chmod 640 "$BACKUP_DIR/$SQL_FILE"
+sudo -u pe-postgres \
+  /opt/puppetlabs/server/apps/postgresql/bin/pg_dumpall \
+  -c -f "$BACKUP_DIR/$SQL_FILE"
 
-# Archive Everything we need
+# Create an archive file with..
+#  - Configuration
+#  - Puppet DB
+#  - Certificates
+#  - Keys
 echo
 echo "Starting Archive of files"
-tar -czf "$FILE_BACKUP" \
+tar -czf "$BACKUP_DIR/$ARCHIVE_FILE" -C "$BACKUP_DIR" \
   /etc/puppetlabs \
   /opt/puppetlabs/server/data/console-services/certs \
   /opt/puppetlabs/server/data/postgresql/9.4/data/certs \
-  "$SQL_BACKUP"
-chmod 640 "$FILE_BACKUP"
+  "$SQL_FILE"
+chmod 640 "$BACKUP_DIR/$ARCHIVE_FILE"
 
 echo
 echo "PE Master backup Complete"
+exit 0
